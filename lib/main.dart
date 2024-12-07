12eff27db43fcf1ebee2af741a24ac12c7846269
siti nur maghfiroh <mghfirrhhh16@gmail.com>
2024-12-08 02:01:07 +0700

    Booking movie app

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ui_design/Fashion%20E-Commerce%20App/Views/fapp_main_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // Root widget of the app
+  @override
+  Widget build(BuildContext context) {
+    return  const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FappMainScreen(),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for hive database
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Hive.initFlutter();
+//   await Hive.openBox("Box");
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: HiveDatabaseFlutter(),
+//     );
+//   }
+// }
